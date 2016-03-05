4a89c02558d51ed2a4e27a1bb55ec074583649ab
paolo <paolobtcx@gmail.com>
2016-03-05 21:28:43 +0100

    Create macchanger.sh

---
+++ macchanger.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#change username and /path/to/icon.png with appropriate values
+
+sudo ifconfig wlp3s0 down && 
+sudo macchanger -r wlp3s0 && 
+sudo ifconfig wlp3s0 up && 
+sudo systemctl restart NetworkManager && 
+newmac=$(macchanger -s wlp3s0 | grep -i 'Current MAC:' | tr -d 'Current MAC ' | tr -d '(unknown)' |sed 's/://') && 
+sudo -u username notify-send -i /path/to/icon.png "Mac address changed successfully. " "newmac"
